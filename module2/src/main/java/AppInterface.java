74bc1a1f6b191561c93e450192550ae7e904c839
Victor Evseenko <smk666@rambler.ru>
2025-08-23 19:40:02 +0300

    create logger and exceptions

--- module2/src/main/java/AppInterface.java
+++ module2/src/main/java/AppInterface.java
@@ -1,39 +1,55 @@
 import java.util.List;
 import java.util.Scanner;
+import org.apache.logging.log4j.*;
 
 public class AppInterface {
     private static final Scanner scanner = new Scanner(System.in);
     private static final UserDao userDao = new UserDaoImpl();
+    private static final Logger logger = LogManager.getLogger(AppInterface.class);
 
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Выберите действие:");
-            System.out.println("1.Показать всех пользователей");
-            System.out.println("2.Показать пользователя по ID");
-            System.out.println("3.Добавить пользователя");
-            System.out.println("4.Обновить пользователя по ID");
-            System.out.println("5.Удалить пользователя по ID");
+        logger.log(Level.INFO, "Старт работы приложения.");
+        boolean runningApp = true;
+        try {
+            while (runningApp) {
+                System.out.println("Выберите действие:");
+                System.out.println("1.Показать всех пользователей");
+                System.out.println("2.Показать пользователя по ID");
+                System.out.println("3.Добавить пользователя");
+                System.out.println("4.Обновить пользователя по ID");
+                System.out.println("5.Удалить пользователя по ID");
+                System.out.println("6.Завершить работу приложения");
 
-            String input = scanner.nextLine();
-            switch (input) {
-                case "1":
-                    getAllUsers();
-                    break;
-                case "2":
-                    getUserById();
-                    break;
-                case "3":
-                    createUser();
-                    break;
-                case "4":
-                    updateUser();
-                    break;
-                case "5":
-                    deleteUser();
-                    break;
-                default:
-                    System.out.println("Некорректный ввод!");
+                String input = scanner.nextLine();
+                switch (input) {
+                    case "1":
+                        getAllUsers();
+                        break;
+                    case "2":
+                        getUserById();
+                        break;
+                    case "3":
+                        createUser();
+                        break;
+                    case "4":
+                        updateUser();
+                        break;
+                    case "5":
+                        deleteUser();
+                        break;
+                    case "6":
+                        runningApp = false;
+                        break;
+                    default:
+                        System.out.println("Некорректный ввод!");
+                }
             }
+        } catch (RuntimeException ex) {
+            logger.log(Level.ERROR, ex.getMessage());
+            System.out.println(ex.getMessage());
+        } finally {
+            HibernateConfiguration.closeSessionFactory();
+            logger.log(Level.INFO, "Завершение работы приложения.");
         }
     }
 
