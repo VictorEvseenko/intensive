8752e1bae07eae65ec7c35b2f306b14fcf002f5f
Victor Evseenko <smk666@rambler.ru>
2025-09-13 19:33:51 +0300

    create tests

--- module5/src/main/java/ru/aston/event/UserEvent.java
+++ module5/src/main/java/ru/aston/event/UserEvent.java
@@ -2,10 +2,12 @@ package ru.aston.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserEvent {
     private String eventType;
