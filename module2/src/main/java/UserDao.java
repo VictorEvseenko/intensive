1ff96e05a7dffbadc81fbcb6aa755179c1dfbaaa
Victor Evseenko <smk666@rambler.ru>
2025-10-04 10:07:51 +0300

    delete module1-2, merge module5-7, few light correct

--- module2/src/main/java/UserDao.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface UserDao {
-    User findById(int id);
-    List<User> findAll();
-    void save(User user);
-    void update(User user);
-    void delete(int id);
-}
